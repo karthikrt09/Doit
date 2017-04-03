dcfbf60388f4d0a5e5620cf56f8af58ea00794ca
karthikrt09 <karthikrt.rt@gmail.com>
2017-04-03 12:54:33 -0700

    Tableview done and 2nd screen Created and connected with main screen

--- Doit/ViewController.swift
+++ Doit/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks: [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTask()
+        tableView.dataSource = self
+        tableView.delegate = self
+    
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.impartant{
+            cell.textLabel?.text = "‼️\(task.name)"
+        }
+        else{
+            cell.textLabel?.text = "📌\(task.name)"
+            
+        }
+        return cell
+    }
+    
+    func makeTask()-> [Task]{
+        let task1 = Task()
+        task1.name = "Go for Run"
+        task1.impartant = false
+        
+        let task2 = Task()
+        task2.name = "Buy Cheese"
+        task2.impartant = true
+        
+        let task3 = Task()
+        task3.name = "Mow Garden"
+        task3.impartant = false
+        
+        return[task1,task2,task3]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pluseTapped(_ sender: Any) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+        
     }
-
 
 }
 
---
+++ Doit/task.swift
@@ -0,0 +1,14 @@
+//
+//  task.swift
+//  Doit
+//
+//  Created by Karthik Thirunavukkarasan on 4/3/17.
+//  Copyright © 2017 Karthik Thiru. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var impartant = false
+}
